d47f0f93a5b98b546cf0ab615a36c3c00727152a
Marcus Markiewicz <me@supermem613.dev>
2023-05-15 09:05:57 -0400

    PR: Add global count
    Let's allow for a global PR count like we do for work items.
    
    This allows for easily checking the PRs that are assigned to you, needs action or whatever.

--- Source/TeamMate/Model/ItemCountSummary.cs
+++ Source/TeamMate/Model/ItemCountSummary.cs
@@ -9,6 +9,7 @@
             this.ResolvedCounter = new Counter();
             this.ClosedCounter = new Counter();
             this.UnknownCounter = new Counter();
+            this.PullRequestsCounter = new Counter();
         }
 
         public Counter GlobalCounter { get; private set; }
@@ -16,6 +17,7 @@
         public Counter ResolvedCounter { get; private set; }
         public Counter ClosedCounter { get; private set; }
         public Counter UnknownCounter { get; private set; }
+        public Counter PullRequestsCounter { get; private set; }
 
         internal void Reset()
         {
@@ -24,6 +26,7 @@
             this.ResolvedCounter.Reset();
             this.ClosedCounter.Reset();
             this.UnknownCounter.Reset();
+            this.PullRequestsCounter.Reset();
         }
     }
 }
